e8d43bc18d78d1739a73c52e0aafd80d3ab355db
cromero2386 <cromero2386@gmail.com>
2024-02-13 11:39:20 -0300

    Adds files for send mail

---
+++ resources/views/mail/sendPostMail.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MundosE.com</title>
+    </head>
+<body>
+    <h1>{{ $details['firstname'] }}</h1>
+    <p>{{ $details['lastname'] }}</p>
+    <p>Se registro Correctamente</p>
+</body>
+</html>
