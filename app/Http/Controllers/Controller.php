38c866242e04edc89d79caa2b634e7e1c26f0140
cromero2386 <cromero2386@gmail.com>
2024-02-17 14:09:49 -0300

    Adds documentation in get-people and set-person

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="Example of crud for MundosE",
+ *      version="1.0.0",
+ *      description="It has to create, read, update and delete people, also list provinces using the external API of argentina.gob.ar.",
+ *      @OA\Contact(
+ *          name="Lic. Romero, Carlos Alberto",
+ *          email="cromero2386@gmail.com",
+ *      ),
+ *      @OA\License(
+ *          name="GNU General Public License (GPL)",
+ *          url="https://www.gnu.org/philosophy/philosophy.html"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -6,6 +6,7 @@ use App\Models\Person;
 use App\Services\PersonService;
 use App\Http\Requests\PersonRequest;
 
+
 class PersonController extends Controller
 {
     public function __construct(
@@ -14,16 +15,124 @@ class PersonController extends Controller
     ) {
     }
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/api/get-people",
+     *     summary="Get a list of people",
+     *     tags={"People"},
+     *     @OA\Response(
+     *          response=200, 
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 @OA\Items(
+     *                      example={
+     *                         "data": {
+     *                            "id": 1,
+     *                             "nombre": "pepe",
+     *                             "apellido": "pepe",
+     *                             "correo": "user@example.com",
+     *                             "provincia": {
+     *                                "id": 1,
+     *                                "nombre": "Bs As"
+     *                              },
+     *                              "created_at": "2024-02-17T16:16:12.729Z",
+     *                              "updated_at": "2024-02-17T16:16:12.729Z",
+     *                              "deleted_at": "2024-02-17T16:16:12.729Z"
+     *                          }  
+     *                      }
+     *                 )
+     *              )
+     *         )
+     *      ),
+     *     @OA\Response(
+     *          response=400, 
+     *          description="Invalid request", 
+     *          @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 @OA\Items( 
+     *                     example={ "error": "Invalid request","error_description": "Bad Request" }
+     *                 )
+     *             )
+     *         )
+     *      )
+     * )
+     * 
      */
+
     public function index()
     {
         return $this->personService::getAllPersons();
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *     path="/api/set-person",
+     *     summary="Adds a new person - with one of example",
+     *     description="Adds a new person",
+     *     tags={"People"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 required={"firstname", "lastname", "email", "province_id"},
+     *                 @OA\Property(
+     *                     property="firstname",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="lastname",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="email",
+     *                     pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="province_id",
+     *                     type="integer"
+     *                 ),
+     *                 example={"firstname": "Carlos", "lastname": "Romero", "email": "example@mail.com", "province_id":18}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Created",
+     *         @OA\JsonContent(
+     *             example={
+     *                "data": {
+     *                   "id": 0,
+     *                    "nombre": "string",
+     *                    "apellido": "string",
+     *                    "correo": "user@example.com",
+     *                    "provincia": {
+     *                       "id": 0,
+     *                       "nombre": "string"
+     *                     },
+     *                     "created_at": "2024-02-17T16:16:12.729Z",
+     *                     "updated_at": "2024-02-17T16:16:12.729Z",
+     *                     "deleted_at": "2024-02-17T16:16:12.729Z"
+     *                 }  
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity",
+     *         @OA\JsonContent(
+     *             example={"message": "El correo ya esta registrado en la base", "errors": {"email": {"El correo ya esta registrado en la base"}}}
+     *         )         
+     *     )
+     * )
      */
+
+
     public function store(PersonRequest $request)
     {
         return $this->personService::createNewPerson($request);
